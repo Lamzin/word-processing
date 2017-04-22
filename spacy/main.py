575b5b8cfcf1c1a9d9be40e936a8526867cea806
oleh <olehtomas@gmail.com>
2017-04-22 14:29:22 +0300

    F: 0.8

--- spacy/main.py
+++ spacy/main.py
@@ -32,10 +32,10 @@ class Classifier(object):
         for cl in clusters:
             for id in cl.train_ids:
                 X.append(self._filter_features(trains[id].features))
-                Y.append(1 if trains[id].is_paraphrase else 0)
+                Y.append(1 if trains[id].is_paraphrase else -1)
         for tr in trains_all:
             X.append(self._filter_features(tr.features))
-            Y.append(1 if tr.is_paraphrase else 0)
+            Y.append(1 if tr.is_paraphrase else -1)
         self.clf = svm.SVC()
         self.clf.fit(X, Y)
 
@@ -90,18 +90,17 @@ def classify(trains, eps, l_max, top=200):
     lvls = {1: set()}
     for feature_id in range(len(trains[0].features)):
         trains = sorted(trains, key=lambda item: item.features[feature_id])
-        def dummy_split(start):
-            h = start
-            while h < trains[-1].features[feature_id]: # max feature value
-                current_train_ids = [
-                    item.array_id
-                    for item in trains
-                    if item.features[feature_id] >= h and item.features[feature_id] <= h + eps
-                ]
-                lvls[1].add(Cluster([feature_id], current_train_ids))
-                h += eps
-        dummy_split(0)
-        dummy_split(eps/2)
+        current_start_with = trains[0].features[feature_id]
+        current = [trains[0].array_id]
+        for item in trains[1:]:
+            if item.features[feature_id] - current_start_with < eps:
+                current.append(item.array_id)
+            else:
+                lvls[1].add(Cluster([feature_id], current))
+                current_start_with = item.features[feature_id]
+                current = [item.array_id]
+        lvls[1].add(Cluster([feature_id], current))
+
     lvls[1] = list(lvls[1])
     lvls[1] = sorted(lvls[1], key=lambda cl: len(cl.train_ids), reverse=True)[:top]
 
@@ -121,33 +120,38 @@ def classify(trains, eps, l_max, top=200):
     return lvls
 
 
-def split_by_clusters(trains):
+def split_by_clusters(trains, cluster_size, l_max, times_item_used):
     clusters = []
     used = {item.array_id: 0 for item in trains}
 
-    for eps in range(1, 40):
-        print "eps: ", float(eps) / 100
-        print "used: ", len([k for k, v in used.items() if v >= 2])
+    for eps in range(1, 400):
+        print "eps: ", float(eps) / 1000
+        print "used: ", len([k for k, v in used.items() if v >= times_item_used])
 
-        trains_ = [item for item in trains if used[item.array_id] < 2]
+        trains_ = [item for item in trains if used[item.array_id] < times_item_used]
         if len(trains_) == 0:
             break
 
-        l = 3
-        cls = classify(trains_, float(eps) / 100, l_max=l, top=100)[l]
+        cls = classify(trains_, float(eps) / 1000, l_max=l_max, top=50)[l_max]
         for cl in cls:
             sz = len(cl.train_ids)
-            if sz > 200:
+            if sz > cluster_size:
                 print cl.feature_ids, len(cl.train_ids)
                 for id in cl.train_ids:
                     used[id]+=1
                 clusters.append(cl)
+    clusters.append(
+        Cluster(
+            [i for i in range(len(trains[0].features))],
+            [item.array_id for item in trains if used[item.array_id] < 2]
+        )
+    )
 
     print len(clusters)
     for cl in clusters:
         print cl.feature_ids, len(cl.train_ids), sorted(list(cl.train_ids))[:10]
 
-    check_used = {}
+    check_used = {i: 0 for i in range(len(trains))}
     for cl in clusters:
         for item in cl.train_ids:
             check_used[item] = check_used[item] + 1 if item in check_used else 1
@@ -159,24 +163,18 @@ def split_by_clusters(trains):
 def test_one_layer_classifier():
     trains_all, trains_pos, trains_neg = get_trains()
 
-    X = [
-        v.features
-        for v in trains_all
-    ]
-    Y = [
-        1 if v.is_paraphrase else 0
-        for v in trains_all
-    ]
+    X = [v.features for v in trains_all]
+    Y = [1 if v.is_paraphrase else -1 for v in trains_all]
     clf = svm.SVC()
     clf.fit(X, Y)
 
     tests = get_tests()
-    count = [[.0, .0], [.0, .0]]
+    count = {-1: {-1: .0, 1: .0}, 1: {-1: .0, 1: .0}}
     for i, answer in enumerate(clf.predict([t.features for t in tests])):
-        count[1 if tests[i].is_paraphrase else 0][answer] += 1
-    precision = count[1][1] / (count[1][1] + count[0][1])
-    recall = count[1][1] / (count[1][1] + count[1][0])
-    accuracy = (count[0][0] + count[1][1]) / len(tests)
+        count[1 if tests[i].is_paraphrase else -1][answer] += 1
+    precision = count[1][1] / (count[1][1] + count[-1][1])
+    recall = count[1][1] / (count[1][1] + count[1][-1])
+    accuracy = (count[-1][-1] + count[1][1]) / len(tests)
     F = 2 * precision * recall / (precision + recall)
     print "one layer classifier"
     print "precision: {}, recall: {}, accuracy: {}, F: {}".format(precision, recall, accuracy, F)
@@ -185,29 +183,37 @@ def test_one_layer_classifier():
 def test_two_layer_classifier():
     trains_all, trains_pos, trains_neg = get_trains()
 
-    # clusters_pos = split_by_clusters(trains_pos)
-    # clusters_neg = split_by_clusters(trains_neg)
-    # with open('clusters.pkl', 'wb') as output:
-    #     pickle.dump(clusters_pos, output, pickle.HIGHEST_PROTOCOL)
-    #     pickle.dump(clusters_neg, output, pickle.HIGHEST_PROTOCOL)
-    # return
-
     def expand_features(clfs, feature_set):
         X = [tr.features for tr in feature_set]
-        Y = [1 if tr.is_paraphrase else 0 for tr in feature_set]
-        for clf in clfs:
+        Y = [1 if tr.is_paraphrase else -1 for tr in feature_set]
+        for i, clf in enumerate(clfs):
+            print "make prediction: {}/{}".format(i + 1, len(clfs))
             predictions = clf.predict(feature_set)
             for i, pr in enumerate(predictions):
                 X[i].append(pr)
-                X[i].extend(clf._filter_features(feature_set[i].features))
+                # X[i].extend(clf._filter_features(feature_set[i].features))
         return X, Y
 
     def make_classifier_lvl2():
-        with open('clusters.pkl', 'rb') as input:
-            clusters_pos = pickle.load(input)
-            clusters_neg = pickle.load(input)
+        clusters_pos = split_by_clusters(trains_pos, 150, 3, 1)
+        clusters_neg = split_by_clusters(trains_neg, 80, 3, 1)
+        # with open('clusters.pkl', 'wb') as output:
+            # pickle.dump(clusters_pos, output, pickle.HIGHEST_PROTOCOL)
+            # pickle.dump(clusters_neg, output, pickle.HIGHEST_PROTOCOL)
+        # with open('clusters.pkl', 'rb') as input:
+            # clusters_pos = pickle.load(input)
+            # clusters_neg = pickle.load(input)
+        print "pos classifiers: {}, neg classifiers: {}".format(len(clusters_pos), len(clusters_neg))
 
         clfs = []
+        # for i, cluster1 in enumerate(clusters_pos):
+        #     for cluster2 in clusters_pos[i+1:]:
+        #         clfs.append(Classifier([cluster1, cluster2], trains_pos, trains_neg))
+        #         print "clfs: ", len(clfs), clfs[-1].feature_ids
+        # for i, cluster1 in enumerate(clusters_neg):
+        #     for cluster2 in clusters_neg[i+1:]:
+        #         clfs.append(Classifier([cluster1, cluster2], trains_neg, trains_pos))
+        #         print "clfs: ", len(clfs), clfs[-1].feature_ids
         for cluster in clusters_pos:
             clfs.append(Classifier([cluster], trains_pos, trains_neg))
         for cluster in clusters_neg:
@@ -215,19 +221,22 @@ def test_two_layer_classifier():
 
         X, Y = expand_features(clfs, trains_all)
         clf_lvl2 = svm.SVC()
+        print "start fit 2 layer. X size: [{}]x[{}], Y size: [{}]".format(len(X), len(X[0]), len(Y))
         clf_lvl2.fit(X, Y)
+        print "finish fit 2 layer"
         return clfs, clf_lvl2
 
     def test(clfs, clf_lvl2):
+        print "start testing..."
         tests = get_tests()
         X, Y = expand_features(clfs, tests)
 
-        count = [[.0, .0], [.0, .0]]
+        count = {-1: {-1: .0, 1: .0}, 1: {-1: .0, 1: .0}}
         for i, pr in enumerate(clf_lvl2.predict(X)):
             count[Y[i]][pr] += 1
-        precision = count[1][1] / (count[1][1] + count[0][1])
-        recall = count[1][1] / (count[1][1] + count[1][0])
-        accuracy = (count[0][0] + count[1][1]) / len(tests)
+        precision = count[1][1] / (count[1][1] + count[-1][1])
+        recall = count[1][1] / (count[1][1] + count[1][-1])
+        accuracy = (count[-1][-1] + count[1][1]) / len(tests)
         F = 2 * precision * recall / (precision + recall)
         print "two layers classifier"
         print "precision: {}, recall: {}, accuracy: {}, F: {}".format(precision, recall, accuracy, F)
